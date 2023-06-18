c8f5d75be92404abac3498c924d936e72f296121
Markcreaddie <67361169+Markcreaddie@users.noreply.github.com>
2023-06-18 04:50:25 -0400

    create HumanPlayer class with unique human methods

---
+++ lib/human_player.rb
@@ -0,0 +1,44 @@
+require_relative "player"
+
+class HumanPlayer<Player
+    def initialize(name)
+        super(name)
+    end
+    def give_input
+        valid_value=false
+        until valid_value==true
+            input= gets.chomp
+            unless input==input.to_i.to_s
+                puts "'#{input}' is not a valid choice. Enter a number between 0-6"
+                next
+            end
+            input = input.to_i
+            unless input>=0 && input <=6
+                puts "'#{input}' is not a valid choice. Enter a number between 0-6"
+                next
+            end
+            valid_value=true
+        end
+        input
+    end
+    def make_pattern()
+        pattern = []
+        input_prompts=[
+            "Enter first number:",
+            "Enter second number:",
+            "Enter third number:",
+            "Enter forth number:"
+        ]
+        input_prompts.each do |prompt|
+            puts prompt
+            player_input=self.give_input
+            pattern.push(player_input)
+        end
+        pattern
+    end
+    def make_guess()
+        self.make_pattern()
+    end
+
+end
+
