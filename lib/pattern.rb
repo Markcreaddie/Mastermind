823ca138d2d166ff732b0a4b283e08d45139eecd
Markcreaddie <67361169+Markcreaddie@users.noreply.github.com>
2023-06-18 04:23:46 -0400

    refactor the get_input method

--- lib/pattern.rb
+++ lib/pattern.rb
@@ -1,21 +1,20 @@
 module Pattern
     def get_input
-        value_value=false
+        valid_value=false
         until valid_value==true
             input= gets.chomp
-            unless pattern==pattern.to_i.to_s
-                puts "Enter a number between 0-6"
+            unless input==input.to_i.to_s
+                puts "'#{input}' is not a valid choice. Enter a number between 0-6"
                 next
             end
             input = input.to_i
             unless input>=0 && input <=6
-                puts "Enter a number between 0-6"
+                puts "'#{input}' is not a valid choice. Enter a number between 0-6"
                 next
             end
             valid_value=true
         end
         input
-    Pattern
     end
 end
 
