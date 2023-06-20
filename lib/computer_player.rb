a9555e4f986ed9bbd87f8e9bfdb0334c10cdcfb9
Markcreaddie <67361169+Markcreaddie@users.noreply.github.com>
2023-06-20 16:13:43 -0400

    Define make_guess logic for computer

--- lib/computer_player.rb
+++ lib/computer_player.rb
@@ -1,10 +1,15 @@
 require_relative("player")
+require_relative("valid_number")
+require_relative("create_array")
 
 class ComputerPlayer<Player
-    attr_accessor :pattern
+    include CreateArray
+    attr_accessor :pattern, :starting_num,:second_num, :third_num, :forth_num
 
     def initialize()
         super("Computer")
+        self.starting_num=ValidNumber.new(0)
+        
     end
     def make_pattern()
         self.pattern=[]
@@ -14,8 +19,22 @@ class ComputerPlayer<Player
         end
         pattern
     end
-    def make_guess()
-        self.make_pattern()
+    def make_guess(feedback)
+        case 
+        when feedback == []
+            return Array.new(4,starting_num.id)
+        when feedback == ['e', 'e', 'e', 'e']
+            starting_num.id+=1
+            return Array.new(4,starting_num.id)
+        when feedback.include?("c") && self.guess.uniq.length==1
+            starting_num.quantity+=feedback.count("c")
+            self.second_num=ValidNumber.new(starting_num.id+1)
+            return self.insert_second_num(starting_num,second_num)
+        when feedback.count("c")==starting_num.quantity && !feedback.include?("w")
+            self.second_num.id+=1
+            return self.insert_second_num(starting_num,second_num)
+
+        end
     end
 
 end
