840a022f09d4c51ac9b1fefc8aef86999bc68405
Markcreaddie <67361169+Markcreaddie@users.noreply.github.com>
2023-06-20 00:28:48 -0400

    Add give_feedback method

--- lib/player.rb
+++ lib/player.rb
@@ -1,7 +1,19 @@
+require_relative "validate"
+
 class Player
-    attr_accessor :name
+    include Validate
+    attr_accessor :name, :feedback, :answer, :guess
     def initialize(name)
         self.name=name
+        self.feedback=[]
     end
+
+    def give_feedback(guess)
+        unvalidated,wrong_order=self.validate_numbers_in_correct_order(answer,guess)
+        missing=self.validate_existing_numbers(unvalidated,wrong_order)
+        self.validate_missing_numbers(missing)
+        feedback
+    end
+
 end
-   
+
