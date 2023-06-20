38ef49d03a241aa2870c36878adbebf43cec176f
Markcreaddie <67361169+Markcreaddie@users.noreply.github.com>
2023-06-20 16:20:21 -0400

    update game logic

--- lib/mastermind.rb
+++ lib/mastermind.rb
@@ -4,7 +4,7 @@ class Mastermind
         self.player1=player1
         self.player2=player2
         self.board=board
-        self.rounds=12
+        self.rounds=8
     end
 
     def correct_answer?
@@ -17,13 +17,13 @@ class Mastermind
         #p player1.answer
         sleep(1)
         (1..rounds).each do |round|
-            puts "Make guess #{round} of 12."
-            player2.guess= player2.make_guess
-            p player1.give_feedback(player2.guess)
+            puts "Make guess #{round} of #{rounds}."
+            player2.guess= player2.make_guess(player1.feedback)
+            p player2.guess 
             unless self.correct_answer?
                 sleep(1)
                 puts "Wrong guess. Try again"
-                player1.feedback=[]
+                feedback= player1.give_feedback(player2.guess)
                 sleep(1)
                 next
             end
