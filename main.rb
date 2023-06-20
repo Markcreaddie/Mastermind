60ec9bb9f0fd785c62d5522bf69f2c8dd7edeb88
Markcreaddie <67361169+Markcreaddie@users.noreply.github.com>
2023-06-20 16:21:44 -0400

    simulate possible game outcomes

--- main.rb
+++ main.rb
@@ -6,5 +6,5 @@ require "./lib/board.rb"
 board= Board.new()
 human_player= HumanPlayer.new("Mark")
 computer_player= ComputerPlayer.new()
-game = Mastermind.new(computer_player,human_player,board)
+game = Mastermind.new(human_player,computer_player,board)
 game.play()
