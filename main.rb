7face878967db218a89a37c32f5ad6d3a3914b99
Markcreaddie <67361169+Markcreaddie@users.noreply.github.com>
2023-06-22 14:06:47 -0400

    Simulate a scenario

--- main.rb
+++ main.rb
@@ -6,5 +6,6 @@ require "./lib/board.rb"
 board= Board.new()
 human_player= HumanPlayer.new("Mark")
 computer_player= ComputerPlayer.new()
-game = Mastermind.new(human_player,computer_player,board)
+#game = Mastermind.new(computer_player,human_player,board)
+game = Mastermind.new(human_player, computer_player,board)
 game.play()
