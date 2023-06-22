1088095d1f6886af9c7ee547be81467030e8697a
Markcreaddie <67361169+Markcreaddie@users.noreply.github.com>
2023-06-22 14:05:51 -0400

    define methods for handling array with 2 numbers

--- lib/create_array.rb
+++ lib/create_array.rb
@@ -10,4 +10,24 @@ module CreateArray
         end
         array
     end
+    def pair_of_two_nums(guess,first_num,second_num)
+        def modify_select_num(select_num,guess)
+            select_num_indices=guess.map.with_index{|num,index| index if  num==select_num.id}.compact
+            select_num.valid_positions.push(select_num_indices[0])
+            select_num_indices.each{ |index| select_num.unvisited_positions.delete(index)}
+        end
+        modify_select_num(first_num,guess)
+        modify_select_num(second_num,guess)
+
+        first_num.unvisited_positions.delete(second_num.valid_positions[0])
+        second_num.unvisited_positions.delete(first_num.valid_positions[0])
+        guess=Array.new(4)
+        def update_guess(guess,select_num)
+            guess[select_num.valid_positions[0]]=select_num.id
+            guess[select_num.unvisited_positions[0]]=select_num.id
+        end
+        update_guess(guess,first_num)
+        update_guess(guess,second_num)
+        guess
+    end
 end
